d4deafdc089aa7302cf4a809062527927e577c35
AmanKaushik975 <91477500+AmanKaushik975@users.noreply.github.com>
2024-03-27 00:08:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+window.addEventListener("resize", () => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    document.getElementById("sizeInfo").innerHTML = `<h1>Width: ${width} Height: ${height}</h1>`;
+});
+
+// Initial setup to display the initial window size
+const initialWidth = window.innerWidth;
+const initialHeight = window.innerHeight;
+document.getElementById("sizeInfo").innerHTML = `<h1>Width: ${initialWidth} Height: ${initialHeight}</h1>`;
